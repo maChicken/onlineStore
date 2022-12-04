fa0a84e29d70883b039d71b31c74a2f16c78e7c7
maChicken <andrew_wenom@mail.ru>
2022-12-04 20:20:46 +0300

    first commit

---
+++ server/models/models.js
@@ -0,0 +1,66 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+    surname: {type: DataTypes.STRING},
+    name: {type: DataTypes.STRING},
+    patronymic: {type: DataTypes.STRING},
+    mail: {type: DataTypes.STRING, unique: true},
+    phone: {type: DataTypes.INTEGER, unique: true},
+    adress: {type: DataTypes.STRING},
+    index: {type: DataTypes.INTEGER},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+    password: {type: DataTypes.STRING},
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+})
+
+const BasketItem = sequelize.define('basketitem', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+})
+
+const Item = sequelize.define('item', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING},
+    composition: {type: DataTypes.STRING},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+})
+
+const RestItem = sequelize.define('restitem', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+})
+
+const Rest = sequelize.define('rest', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIIncrement: true},
+    size: {type: DataTypes.STRING, allowNull: false},
+    rest: {type: DataTypes.INTEGER, allowNull: false},
+})
+
+User.hasOne(Basket)
+Basket.hasOne(User)
+
+Basket.hasMany(BasketItem)
+BasketItem.belongsTo(Basket)
+
+Item.hasMany(BasketItem)
+BasketItem.belongsTo(Item)
+
+Item.hasMany(RestItem)
+RestItem.belongsTo(Item)
+
+Rest.hasMany(RestItem)
+RestItem.belongsTo(Rest)
+
+module.exports = {
+    User,
+    Basket,
+    BasketItem,
+    Item,
+    RestItem,
+    Rest
+}
